c3a3545d3b85fe3c8da60fe834aa1610af485716
Jeewantha Dilshan <106438829+GihanJeewantha@users.noreply.github.com>
2022-12-08 22:42:14 +0530

    JavaScript variables
    Getting User Input Using Prompt key and display them using Console.log

---
+++ Script.js
@@ -0,0 +1,17 @@
+console.log('Hello world');
+
+//Single line comment
+
+/* Multi line comment */
+
+//declare variable and assign value to it(Ussing "var" key word to declare varible)
+var userName = "Hello JD";
+var userName1;
+
+console.log (userName);
+
+//Getting User input Ussing "Prompt" key word
+userName1 = prompt("Enter User Name: ");
+console.log(userName1);
+
+var username,address;
