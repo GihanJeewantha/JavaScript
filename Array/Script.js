496b1a00fa5f80e3902af13b329b1423d3686272
Jeewantha Dilshan <106438829+GihanJeewantha@users.noreply.github.com>
2022-12-19 23:43:31 +0530

    Array

---
+++ Array/Script.js
@@ -0,0 +1,24 @@
+var marks = [88,40,60,55];
+console.log(marks);
+
+marks.length
+
+console.log(marks[0]);
+
+for(var i=0; i<marks.length; i++ ){
+    console.log(marks[i]);
+}
+
+//empty Array
+
+var arr = []
+
+arr.push(15)
+
+arr.push(70)
+
+//use push to add 
+
+arr.pop(1)
+
+//use pop to remove value from array
