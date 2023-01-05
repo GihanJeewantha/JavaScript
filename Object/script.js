b61e6219447b3faca44c805815f0c1355866556d
Jeewantha Dilshan <106438829+GihanJeewantha@users.noreply.github.com>
2023-01-05 14:14:44 +0530

    Object

---
+++ Object/script.js
@@ -0,0 +1,15 @@
+//create java script object
+
+/*
+1. first create variable 
+2. then add attribute 
+
+*/
+
+let Student = {
+  name: "gihan",
+  age: 23,
+  subject: "Information technology",
+};
+
+console.log(Student);
